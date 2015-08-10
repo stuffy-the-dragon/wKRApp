1073550b0b3a042cb3396bf9bc87999fbbf06ce5
stuffy-the-dragon <stuffy-the-dragon@users.noreply.github.com>
2015-08-10 14:02:11 +0200

    feat(view): Add kra skeleton view

--- wKRApp/views.py
+++ wKRApp/views.py
@@ -1,5 +1,5 @@
 from wKRApp import app
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from ipdb import set_trace
 
 @app.route('/', methods=['GET', 'POST'])
@@ -7,3 +7,9 @@ def index():
     if request.method == 'POST':
         return render_template('team.html')
     return render_template('signin.html')
+
+@app.route('/kra', methods=['GET', 'POST'])
+def kra():
+    if request.method == 'POST':
+        return render_template('kra.html')
+    return render_template('kra.html')

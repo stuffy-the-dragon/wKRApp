8cfdde902c6f57bab07e358bf0700734bdef0143
stuffy-the-dragon <stuffy-the-dragon@users.noreply.github.com>
2015-08-13 23:13:19 +0200

    refactor(views): PEP 8 compliant

--- wKRApp/views.py
+++ wKRApp/views.py
@@ -2,11 +2,13 @@ from wKRApp import app
 from flask import Flask, render_template, url_for, request, redirect
 from ipdb import set_trace
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         return render_template('team.html')
     return render_template('signin.html')
+
 
 @app.route('/kra', methods=['GET', 'POST'])
 def kra():

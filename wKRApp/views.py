af2c8a59ad7333a050210b8c8aa2c16ef529680b
Meshack Shabalala <bafana5@gmail.com>
2015-10-10 22:08:35 +0200

    feature(routes): Add routes in views.py
    - Added routes in the views.py source
    - The routes lead to new_role.html, new_user.html, user_roles.html,
      users.html, admin.html and
      workflow.html

--- wKRApp/views.py
+++ wKRApp/views.py
@@ -15,3 +15,31 @@ def kra():
     if request.method == 'POST':
         return render_template('kra.html')
     return render_template('kra.html')
+
+
+@app.route('/new_role', methods=['GET', 'POST'])
+def new_role():
+    if request.method == 'POST':
+        return render_template('new_role.html')
+    return render_template('new_role.html')
+
+
+@app.route('/new_user', methods=['GET', 'POST'])
+def new_user():
+    if request.method == 'POST':
+        return render_template('new_user.html')
+    return render_template('new_user.html')
+
+
+@app.route('/user_roles', methods=['GET', 'POST'])
+def user_roles():
+    if request.method == 'POST':
+        return render_template('user_roles.html')
+    return render_template('user_roles.html')
+
+
+@app.route('/workflow', methods=['GET', 'POST'])
+def workflow():
+    if request.method == 'POST':
+        return render_template('workflow.html')
+    return render_template('workflow.html')

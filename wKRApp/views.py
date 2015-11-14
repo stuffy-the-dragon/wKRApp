7eaf6beba4afd9ed46fa7a0e4ca140e0a564199c
Meshack <mshabalala@csir.co.za>
2015-11-14 12:01:29 +0200

    style(GET method): Remove GET method
    - Removed the GET methods on the admin, users, user_roles, and workflow
      app route
    - The GET method is implicitly specified by default

--- wKRApp/views.py
+++ wKRApp/views.py
@@ -2,7 +2,6 @@ from wKRApp import app
 from flask import Flask, render_template, url_for, request, redirect
 from ipdb import set_trace
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -20,32 +19,29 @@ def kra():
     return render_template('kra.html')
 
 # THIS MAY BE A PROBLEM SINCE ANYONE CAN NAVIGATE TO THE ADMIN AND ALL TEMPLATES BELOW  WITHOUT A SIGNIN
-@app.route('/admin', methods=['GET'])
+@app.route('/admin')
 def admin():
     return render_template('admin.html')
 
 
-@app.route('/users', methods=['GET'])
+@app.route('/users')
 def users():
     return render_template('users.html')
 
 
-@app.route('/user_roles', methods=['GET'])
+@app.route('/user_roles')
 def user_roles():
     return render_template('user_roles.html')
 
 
-@app.route('/workflow', methods=['GET'])
+@app.route('/workflow')
 def workflow():
     return render_template('workflow.html')
 
 
-@app.route('/career_ladders', methods=['GET'])
+@app.route('/career_ladders')
 def career_ladders():
     return render_template('career_ladders.html')
-
-
-
 
 
 @app.route('/new_role', methods=['GET', 'POST'])
@@ -53,7 +49,6 @@ def new_role():
     if request.method == 'POST':
         return render_template('new_role.html')
     return render_template('new_role.html')
-
 
 @app.route('/new_user', methods=['GET', 'POST'])
 def new_user():

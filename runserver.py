d043eef098be68690b9d6cd5790b667cdb2d825b
Meshack <mshabalala@csir.co.za>
2015-11-14 16:38:23 +0200

    style(comments): Add comments about security issue
    - Added comments about two security issues relating to the session secret key

--- runserver.py
+++ runserver.py
@@ -1,3 +1,6 @@
 from wKRApp import app
-app.secret_key = "my precious" # 2 security flaws, need to sort out
+ # 2 security flaws, need to sort out
+ # 		1. the key should be randomy generated
+ # 		2. the key should be set in a config file that is then imported in.
+app.secret_key = "my precious"
 app.run(debug=True)

b3f3f7d41f4e83e58980c2442d21992d1e9517d2
Meshack <mshabalala@csir.co.za>
2015-11-16 17:35:12 +0200

    chore(runserver): remove app object instance
    - Removed app object instance
    - Removed the session key creation

--- runserver.py
+++ runserver.py
@@ -1,6 +1,2 @@
 from wKRApp import app
- # 2 security flaws, need to sort out
- # 		1. the key should be randomy generated
- # 		2. the key should be set in a config file that is then imported in.
-app.secret_key = "my precious"
 app.run(debug=True)

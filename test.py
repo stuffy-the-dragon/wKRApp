cffe368bac9f7f85841f4b87116131c286b91937
Meshack <mshabalala@csir.co.za>
2015-11-18 16:34:30 +0200

    test(login): Test that login is required
    Tested that the login is required on first page

--- test.py
+++ test.py
@@ -8,11 +8,16 @@ class FlaskTestCase(unittest.TestCase):
 		response = tester.get('/', content_type='html/text')
 		self.assertEqual(response.status_code, 200)
 
-	# Ensure that the login page kloads correctly
+	# Ensure that the login page loads correctly
 	def test_signin_page_loads(self):
 		tester = app.test_client(self)
 		response = tester.get('/', content_type='html/text')
 		self.assertTrue(b'Please sign in' in response.data)
+		
+   # Ensure that main page requires user login
+    def test_main_route_requires_login(self):
+        response = self.client.get('/', follow_redirects=True)
+        self.assertIn(b'Please log in to access this page', response.data)
 
 if __name__ == '__main__':
 	unittest.main()
